d885e0e532106e10fe0d7b7b14179006f6e38a5d
imochoa <18612100+imochoa@users.noreply.github.com>
2025-05-30 18:06:48 +0200

    set up dev environment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ansible-playbooks
+# Ansible Pull tutorial
+
+![Ansible Logo](https://www.learnlinux.tv/wp-content/uploads/2020/12/ansible-e1607524003363.png)
+
+This repository was used for the Ansible Pull tutorial from the LearnLinux.TV YouTube channel.
+
+You can view the original video for this repository [here](https://youtu.be/sn1HQq_GFNE).
+
+# Bootstrap
+1. pull github repo? `sudo apt install -y git`
+2. Install ansible `sudo apt install -y ansible`
+
+
+sudo ansible-pull -U https://github.com/<org>/repo.git
---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "ansiblePull";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    inputs@{
+      self,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate =
+          pkg: builtins.elem (inputs.nixpkgs.lib.getName pkg) [ "google-chrome" ];
+      };
+    in
+    {
+
+      # nix.nixPath = [ "nixpkgs=${pkgs}" ];
+      devShells.${system}.default = pkgs.mkShell {
+        # env vars
+        UV_PYTHON_DOWNLOADS = "never";
+        packages = with pkgs; [
+          ansible
+          ansible-lint
+          # ansible-vault
+
+        ];
+        # inputsFrom = [
+        #   # pkgs.hello
+        #   # pkgs.gnutar
+        # ];
+        # shellHook = ''
+        #   uv tree
+        # '';
+
+      };
+
+      packages.${system} = {
+        inherit (self.devShells.${system}) default;
+      };
+    };
+}
